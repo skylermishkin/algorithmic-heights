1c8a2b0dd1cd27cb27f2cdc5cf957457942bf0f4
skylermishkin <meestermishkin@gmail.com>
2015-10-01 17:40:31 -0600

    fixed off by one mistake

--- src/fibo/main.py
+++ src/fibo/main.py
@@ -2,6 +2,6 @@ def fib(n):
     a = [0, 1]
     if n < 0:
         raise ValueError("Argument must be positive")
-    for i in range(2, n):
-            a.append(a[i-1] + a[i - 2])
+    for i in range(2, n+1):
+            a.append(a[i-1] + a[i-2])
     return a[n]
